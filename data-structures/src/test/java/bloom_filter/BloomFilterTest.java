0935d8c22b535ecab8375a34aeaae7372706cdd5
DoubleW2w <1049951363@qq.com>
2023-12-24 12:33:52 +0800

    添加布隆过滤器实现

---
+++ data-structures/src/test/java/bloom_filter/BloomFilterTest.java
@@ -0,0 +1,72 @@
+package bloom_filter;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.BitSet;
+import java.util.Random;
+
+/**
+ * @author: DoubleW2w
+ * @description:
+ * @date: 2023/12/24 12:27
+ * @project: hello-java-algo
+ */
+public class BloomFilterTest {
+
+    private Logger logger = LoggerFactory.getLogger(BloomFilterTest.class);
+
+    @Test
+    public void test() {
+        String val00 = "小傅哥";
+        String val01 = "https://bugstack.cn";
+        String val02 = "https://github.com/fuzhengwei/CodeGuide";
+        String val03 = "https://github.com/fuzhengwei";
+
+        BloomFilter filter = new BloomFilter(Integer.MAX_VALUE, new int[]{7, 19, 43, 77});
+        filter.add(val00);
+        filter.add(val01);
+        filter.add(val02);
+
+        logger.info("测试结果 val00：{} 布隆过滤器：{}", val00, filter.contains(val00));
+        logger.info("测试结果 val01：{} 布隆过滤器：{}", val01, filter.contains(val01));
+        logger.info("测试结果 val02：{} 布隆过滤器：{}", val02, filter.contains(val02));
+        logger.info("测试结果 val02：{} 布隆过滤器：{}", val03, filter.contains(val03));
+    }
+
+    @Test
+    public void test_BitSet() {
+        BitSet bits = new BitSet(8);
+        bits.set("谢飞机".hashCode() & (8 - 1), true);
+//        bits.set("金毛狮王".hashCode() & (8 - 1), true);
+//        bits.set("杨左使".hashCode() & (8 - 1), true);
+
+        boolean exist = bits.get("成昆".hashCode() & (8 - 1));
+        logger.info("测试结果：{}", exist);
+
+        System.out.println("金毛狮王".hashCode() & (8 - 1));
+        System.out.println("杨左使".hashCode() & (8 - 1));
+        System.out.println("拎瓢冲".hashCode() & (8 - 1));
+        System.out.println("谢飞机".hashCode() & (8 - 1));
+        System.out.println("成昆".hashCode() & (8 - 1));
+
+        System.out.println(Integer.toBinaryString(6));
+    }
+
+    @Test
+    public void test_random() {
+        BloomFilter filter = new BloomFilter(Integer.MAX_VALUE, new int[]{7, 19, 43, 77});
+        for (int i = 0; i < 8000; i++) {
+            StringBuilder code = new StringBuilder();
+            for (int j = 0; j < 6; j++) {
+                code.append(new Random().nextInt(9));
+            }
+            if (filter.contains(code.toString())) {
+                System.out.println("碰撞：" + code);
+                continue;
+            }
+            filter.add(code.toString());
+        }
+    }
+}
